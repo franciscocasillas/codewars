42bfc391a00f9d720c42ffab031ca0fad5ba65a6
Francisco J. Casillas <chiccasillas@gmail.com>
2022-05-16 19:31:15 -0500

    Kata 0005 solved

---
+++ 0005.js
@@ -0,0 +1,11 @@
+function positiveSum(arr) {
+	let total = 0;
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] > 0) {
+			total += arr[i];
+		}
+	}
+	console.log(total);
+}
+
+positiveSum([-1, -2, -3, -4, -5]);
