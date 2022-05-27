889ac7ee81af66236cf914978ffcd2a99415539f
Francisco J. Casillas <chiccasillas@gmail.com>
2022-05-27 10:35:32 -0500

    Kata 0025 solved

---
+++ 0025.js
@@ -0,0 +1,9 @@
+//https://www.codewars.com/kata/53af2b8861023f1d88000832/train/javascript
+
+function areYouPlayingBanjo(name) {
+	return name[0].toLowerCase() === "r"
+		? name + " plays banjo"
+		: name + " does not play banjo";
+}
+
+areYouPlayingBanjo("Adam");
