fee8576c120d2a3e444938af161f1eef583eb698
Francisco Casillas <chiccasillas@gmail.com>
2022-05-28 12:45:20 -0500

    Kata 0028 solved

---
+++ 0028.js
@@ -0,0 +1,7 @@
+//https://www.codewars.com/kata/57a2013acf1fa5bfc4000921/solutions/javascript
+
+class Kata {
+	static getVolumeOfCuboid(length, width, height) {
+		return length * width * height;
+	}
+}
