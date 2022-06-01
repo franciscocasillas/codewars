2614b29424576f6bf2a3cb62a1e2262e4c4a050c
Francisco Casillas <chiccasillas@gmail.com>
2022-06-01 18:02:10 -0500

    Kata 0037 solved

---
+++ 0037.js
@@ -0,0 +1,11 @@
+//https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/javascript
+
+function even_or_odd(number) {
+	if (number % 2 === 0) {
+		console.log("Even");
+	} else {
+		console.log("Odd");
+	}
+}
+
+even_or_odd(30);
