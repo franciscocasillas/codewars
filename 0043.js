0d1b9f8ce4847cd904ec6848b9cf8285595c0178
Francisco Casillas <chiccasillas@gmail.com>
2022-06-03 16:37:41 -0500

    Kata 0043 solved

---
+++ 0043.js
@@ -0,0 +1,6 @@
+//www.codewars.com/kata/55f73be6e12baaa5900000d4/train/javascript
+
+https: function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+	let totalGoals = laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+	console.log(totalGoals);
+}
