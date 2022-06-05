b08366cea32d0cb3593de2ee1d1b8add6cdd2f6a
Francisco Casillas <chiccasillas@gmail.com>
2022-06-05 15:05:56 -0500

    Kata 0046 solved

---
+++ 0046.js
@@ -0,0 +1,18 @@
+//https://www.codewars.com/kata/54a91a4883a7de5d7800009c/train/javascript
+
+function incrementString(strng) {
+	let number = strng.match(/\d+/g);
+	if (number === null) {
+		strng = strng + "1";
+		console.log(strng);
+	} else {
+		let array = strng.split("");
+		let num = array.findIndex((element) => !isNaN(element));
+		number = Number(number) + 1;
+		array.splice(num);
+		array = array.join("") + number;
+		console.log(array);
+	}
+}
+
+incrementString("foo");
