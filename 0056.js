c8f8a7aa18120316b497f8fc367b9fc955722d02
Francisco Casillas <chiccasillas@gmail.com>
2022-06-09 13:30:40 -0500

    Kata 0056 solved

---
+++ 0056.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/55fab1ffda3e2e44f00000c6/train/javascript
+
+function cockroachSpeed(s) {
+	cmxs = Math.floor(s * 27.777778);
+	console.log(cmxs);
+}
+
+cockroachSpeed(0);
