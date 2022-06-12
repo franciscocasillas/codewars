2e7809c53729e43169d8249a5d71f6773a420fe7
Francisco Casillas <chiccasillas@gmail.com>
2022-06-12 15:38:32 -0500

    Kata 0061 solved

---
+++ 0061.js
@@ -0,0 +1,11 @@
+//https://www.codewars.com/kata/58649884a1659ed6cb000072/train/javascript
+
+function updateLight(current) {
+	return current === "green"
+		? "yellow"
+		: current === "yellow"
+		? "red"
+		: "green";
+}
+
+updateLight("green");
