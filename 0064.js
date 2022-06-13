a40660ed46ef42bf51d7cd5d10fba3a973690f18
Francisco Casillas <chiccasillas@gmail.com>
2022-06-13 11:27:04 -0500

    Kata 0064 solved

---
+++ 0064.js
@@ -0,0 +1,11 @@
+//https://www.codewars.com/kata/55ecd718f46fba02e5000029/train/javascript
+
+function between(a, b) {
+	let arr = [];
+	for (let i = a; i <= b; i++) {
+		arr.push(i);
+	}
+	console.log(arr);
+}
+
+between(-2, 2);
