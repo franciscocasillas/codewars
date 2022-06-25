4503e5d2716cd78493b9399dbb6c4f388de7c498
Francisco Casillas <chiccasillas@gmail.com>
2022-06-25 10:52:11 -0500

    Kata 0085 solved

---
+++ 0085.js
@@ -0,0 +1,8 @@
+//https://www.codewars.com/kata/5168bb5dfe9a00b126000018/train/javascript
+
+function reverseStr(str) {
+	let reversed = str.split("").reverse();
+	console.log(reversed.join(""));
+}
+
+reverseStr("Hello");
