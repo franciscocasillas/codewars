2a62c56dd5705781d4c06515a8698130bf99cd23
Francisco Casillas <chiccasillas@gmail.com>
2022-06-29 19:15:05 -0500

    Kata 0089 solved

---
+++ 0089.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/57202aefe8d6c514300001fd/train/javascript
+
+function saleHotdogs(n) {
+	return n >= 5 && n < 10 ? n * 95 : n >= 10 ? n * 90 : n * 100;
+}
+
+saleHotdogs(10);
