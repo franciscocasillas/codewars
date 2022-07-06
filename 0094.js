3c7eb0b8e4082cf332415f78c360bc0f7430a1b7
Francisco Casillas <chiccasillas@gmail.com>
2022-07-06 10:49:09 -0500

    Kata 0094 solved

---
+++ 0094.js
@@ -0,0 +1,8 @@
+//https://www.codewars.com/kata/557cd6882bfa3c8a9f0000c1/train/javascript
+
+function getAge(inputString) {
+	let arr = inputString.split(" ");
+	console.log(Number(arr[0]));
+}
+
+getAge("40 years old");
