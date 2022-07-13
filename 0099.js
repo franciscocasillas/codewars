2b016095af89c8a33768a35738fc1bbe948f8a46
Francisco Casillas <chiccasillas@gmail.com>
2022-07-13 09:37:36 -0500

    Kata 0099 solved

---
+++ 0099.js
@@ -0,0 +1,11 @@
+//https://www.codewars.com/kata/555a67db74814aa4ee0001b5/train/javascript
+
+function testEven(n) {
+	if (n % 2 === 0) {
+		console.log(true);
+	} else {
+		console.log(false);
+	}
+}
+
+testEven(-4);
