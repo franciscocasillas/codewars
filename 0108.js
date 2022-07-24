98ca4578d40edf82b389071357118d318990ae72
Francisco Casillas <chiccasillas@gmail.com>
2022-07-23 19:49:48 -0500

    Kata 108 solved

---
+++ 0108.js
@@ -0,0 +1,6 @@
+//https://www.codewars.com/kata/55ca77fa094a2af31f00002a/train/javascript
+
+var laLigaGoals;
+var championsLeagueGoals;
+var copaDelReyGoals;
+var totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals;
