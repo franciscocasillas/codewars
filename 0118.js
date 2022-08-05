80898f69a8904379a2fbb838d0c6df3c51dec978
Francisco Casillas <chiccasillas@gmail.com>
2022-08-05 08:02:50 -0500

    Kata 0118 solved

---
+++ 0118.js
@@ -0,0 +1,9 @@
+//https://www.codewars.com/kata/5966e33c4e686b508700002d/train/javascript
+
+function sumStr(a, b) {
+	let sum = +a + +b;
+	let strNum = sum.toString();
+	console.log(strNum);
+}
+
+sumStr("4", "5");
