0743dcb5e90004543baa7a9f736c14b92be9d3d0
Francisco Casillas <chiccasillas@gmail.com>
2022-09-19 12:16:44 -0500

    Kata 0148 solved

---
+++ 0148.js
@@ -0,0 +1,8 @@
+//https://www.codewars.com/kata/545afd0761aa4c3055001386/train/javascript
+
+function take(arr, n) {
+	let finalArray = arr.slice(0, n);
+	console.log(finalArray);
+}
+
+take([0, 1, 2, 3, 5, 8, 13], 3);
