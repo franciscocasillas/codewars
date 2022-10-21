12a8bebcc366b2ce23b785940b65fd4a5ab3a31b
Francisco Casillas <chiccasillas@gmail.com>
2022-10-21 11:55:50 -0500

    Kata 0169 solved

---
+++ 0169.js
@@ -0,0 +1,9 @@
+//https://www.codewars.com/kata/545991b4cbae2a5fda000158/train/javascript
+
+function include(arr, item) {
+	if (arr.includes(item) === true) {
+		return true;
+	} else {
+		return false;
+	}
+}
