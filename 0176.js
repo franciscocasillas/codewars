38d498170207933338829108f0572f079641f402
Francisco Casillas <chiccasillas@gmail.com>
2022-11-10 16:06:37 -0600

    Kata 0176 solved

---
+++ 0176.js
@@ -0,0 +1,13 @@
+//https://www.codewars.com/kata/56170e844da7c6f647000063/train/javascript
+
+function peopleWithAgeDrink(old) {
+	return old < 14
+		? "drink toddy"
+		: old < 18
+		? "drink coke"
+		: old < 21
+		? "drink beer"
+		: "drink whisky";
+}
+
+peopleWithAgeDrink(12);
