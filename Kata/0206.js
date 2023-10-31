5e5878a8262b5b9fea0f85644975c7d8f607b0b2
Francisco J. Casillas <chiccasillas@gmail.com>
2023-10-31 15:21:06 -0600

    Create pseudocode for new problem

---
+++ Kata/0206.js
@@ -0,0 +1,14 @@
+//Complete the findNextSquare method that finds the next integral perfect square after the one passed as a parameter. Recall that an integral perfect square is an integer n such that sqrt(n) is also an integer.
+// If the parameter is itself not a perfect square then -1 should be returned. You may assume the parameter is non-negative.
+
+function findNextSquare(sq) {
+	//check if number is a perfect square
+	//conditional
+	//if number is perfect square, find next number
+	//
+	//if number is not perfect square, return -1
+}
+
+findNextSquare(121); // 144
+findNextSquare(625); // 676
+findNextSquare(114); //-1
